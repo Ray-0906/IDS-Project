7e779a59ebc4c0d152231b239baee8d109a4de22
Ray-0906 <astra3716@gmail.com>
2025-09-15 19:50:07 +0530

    chk1

--- dashboard/server/controllers/authControllers.js
+++ dashboard/server/controllers/authControllers.js
@@ -67,3 +67,12 @@ export const verifyUser = (req, res) => {
     res.status(200).json({ message: "User verified", user: decoded });
   });
 };
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("-password");  
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching users", error });
+  } 
+};
